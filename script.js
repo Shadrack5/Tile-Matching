6521dc6236352f65c9804f11dcf5915a345be514
Shadrack5 <shadrackkibet100@gmail.com>
2023-05-22 14:48:45 +0300

    added start button

--- script.js
+++ script.js
@@ -1,79 +1,60 @@
-const card = document.querySelectorAll('.cell')
-const front = document.querySelectorAll('.front')
-const container = document.querySelector('.container')
-const score = document.querySelector('.score span')
+const card = document.querySelectorAll('.cell');
+const front = document.querySelectorAll('.front');
+const container = document.querySelector('.container');
+const score = document.querySelector('.score span');
+const movesDisplay = document.querySelector('.moves span');
 
+let moves = 0;
 
+suffleImage();
+clicking();
 
-suffleImage()
-clicking()
-function suffleImage(){
-
-
-    card.forEach(c=>{
-
-        const num = [...Array(card.length).keys()]
-        const random = Math.floor(Math.random()*card.length)
-
-        c.style.order = num[random]
-    })
+function suffleImage() {
+  card.forEach(c => {
+    const num = [...Array(card.length).keys()];
+    const random = Math.floor(Math.random() * card.length);
+    c.style.order = num[random];
+  });
 }
 
+function clicking() {
+  for (let i = 0; i < card.length; i++) {
+    front[i].classList.add('show');
+    setInterval(() => {
+      front[i].classList.remove('show');
+    }, 2000);
 
-function clicking(){
+    card[i].addEventListener('click', () => {
+      front[i].classList.add('flip');
+      const flippedCard = document.querySelectorAll('.flip');
 
-    for(let i =0; i<card.length; i++){
-
-
-        front[i].classList.add('show')
+      if (flippedCard.length == 2) {
+        container.style.pointerEvents = 'none';
 
         setInterval(() => {
-            front[i].classList.remove('show')
-        }, 2000);
+          container.style.pointerEvents = 'all';
+        }, 1000);
 
-        card[i].addEventListener('click' ,()=>{
+        match(flippedCard[0], flippedCard[1]);
+      }
 
-            front[i].classList.add('flip')
-           const filppedCard = document.querySelectorAll('.flip')
-
-            if(filppedCard.length == 2){
-
-                container.style.pointerEvents ='none'
-                
-                setInterval(() => {
-                    
-                    container.style.pointerEvents ='all'
-                }, 1000);
- 
-                match(filppedCard[0] , filppedCard[1])
-            }
-        })
-    }
+      moves += 1;
+      movesDisplay.textContent = moves;
+    });
+  }
 }
 
-
-
-
-function match(cardOne , cardTwo){
-
-    if(cardOne.dataset.index == cardTwo.dataset.index){
-
-        score.innerHTML = parseInt(score.innerHTML) + 1
-       
-        cardOne.classList.remove('flip') 
-        cardTwo.classList.remove('flip') 
-
-
-        cardOne.classList.add('match')
-        cardTwo.classList.add('match')
-
-
-    }else{
-
-        setTimeout(() => {
-            
-            cardOne.classList.remove('flip') 
-            cardTwo.classList.remove('flip') 
-        }, 1000);
-    }
+function match(cardOne, cardTwo) {
+  if (cardOne.dataset.index == cardTwo.dataset.index) {
+    score.innerHTML = parseInt(score.innerHTML) + 1;
+    cardOne.classList.remove('flip');
+    cardTwo.classList.remove('flip');
+    cardOne.classList.add('match');
+    cardTwo.classList.add('match');
+  } else {
+    setTimeout(() => {
+      cardOne.classList.remove('flip');
+      cardTwo.classList.remove('flip');
+    }, 1000);
+  }
 }
